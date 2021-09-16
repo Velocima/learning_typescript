63cf2ea0d2e57f9d3d4a84a159ec3c93d11a19b5
Velo <hartley.max@outlook.com>
2021-09-16 09:42:08 +0100

    add interface test

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ const printHello = () => {
     console.log('Hello Typescript!');
 };
 printHello();
+let age = 5;
+const mocha = {
+    name: 'Mocha',
+    color: 'Dark brown',
+    age: 1,
+};
+const chai = {
+    name: 'Chai',
+    color: 'Light brown',
+    age: 1,
+};
+const java = {
+    name: 'Java',
+    color: 'Black',
+    age: 1,
+};
+const cats = [mocha, java, chai];
+function getCatNames() {
+    let names = '';
+    for (let cat of cats) {
+        names += `${cat.name}, `;
+    }
+    return names.substring(0, names.length - 2);
+}
+console.log(`I have ${cats.length} cats. Their names are ${getCatNames()}.`);
--- app.ts
+++ app.ts
@@ -6,3 +6,41 @@ const printHello = () => {
 };
 
 printHello();
+
+let age: number = 5;
+
+interface Cat {
+	name: string;
+	color: string;
+	age: number;
+}
+
+const mocha: Cat = {
+	name: 'Mocha',
+	color: 'Dark brown',
+	age: 1,
+};
+
+const chai: Cat = {
+	name: 'Chai',
+	color: 'Light brown',
+	age: 1,
+};
+
+const java: Cat = {
+	name: 'Java',
+	color: 'Black',
+	age: 1,
+};
+
+const cats: Cat[] = [mocha, java, chai];
+
+function getCatNames() {
+	let names: string = '';
+	for (let cat of cats) {
+		names += `${cat.name}, `;
+	}
+	return names.substring(0, names.length - 2);
+}
+
+console.log(`I have ${cats.length} cats. Their names are ${getCatNames()}.`);
