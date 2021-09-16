68707fed1415f4a99d42613f3a6f7698e2960309
Velo <hartley.max@outlook.com>
2021-09-16 09:17:53 +0100

    create basic app and tsconfig

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log('Hello World!');
+const printHello = () => {
+    console.log('Hello World!');
+    console.log('Hello Typescript!');
+};
---
+++ app.ts
@@ -0,0 +1,6 @@
+console.log('Hello World!');
+
+const printHello = () => {
+	console.log('Hello World!');
+	console.log('Hello Typescript!');
+};
